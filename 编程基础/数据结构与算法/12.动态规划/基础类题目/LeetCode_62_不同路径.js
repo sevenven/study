12cb3b8988976264c84eb470b94df2d2ed6b081b
suhuifen <1076931904@qq.com>
2025-07-17 22:57:06 +0800

    update

--- 编程基础/数据结构与算法/12.动态规划/基础类题目/LeetCode_62_不同路径.js
+++ 编程基础/数据结构与算法/12.动态规划/基础类题目/LeetCode_62_不同路径.js
@@ -17,23 +17,23 @@
 		5.填充dp表
 */
 var uniquePaths = function (m, n) {
-  // const dp = Array.from({ length: m }, () => []);
-  // for (let i = 0; i < m; i++) dp[i][0] = 1;
-  // for (let j = 1; j < n; j++) dp[0][j] = 1;
-  // for (let i = 1; i < m; i++) {
-  //   for (j = 1; j < n; j++) {
-  //     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-  //   }
-  // }
-  // return dp[m - 1][n - 1];
-  // 状态压缩
-  const dp = Array(n).fill(1);
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[j] = dp[j] + dp[j - 1];
-    }
-  }
-  return dp[n - 1];
+	// const dp = Array.from({ length: m }, () => []);
+	// for (let i = 0; i < m; i++) dp[i][0] = 1;
+	// for (let j = 1; j < n; j++) dp[0][j] = 1;
+	// for (let i = 1; i < m; i++) {
+	// 	for (j = 1; j < n; j++) {
+	// 		dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+	// 	}
+	// }
+	// return dp[m - 1][n - 1];
+
+	const dp = Array(n).fill(1);
+	for (let i = 1; i < m; i++) {
+		for (let j = 1; j < n; j++) {
+			dp[j] = dp[j] + dp[j - 1]; // 状态压缩 1.dp[j]保存上一行的dp[i - 1][j] 2.dp[j - 1]是当前行已更新的dp[i][j - 1]
+		}
+	}
+	return dp[n - 1];
 };
 
 console.log(uniquePaths(3, 2)); // 3
